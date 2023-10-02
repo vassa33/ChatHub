9857299a30cc0d9f3e3fa0404247414862e6c8eb
vassa33 <peggyongoma@gmail.com>
2023-10-02 22:27:04 +0300

    Authentication

---
+++ lib/shared/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = "1:692680391850:web:968186022fab340235e4d3";
+  static String apiKey = "AIzaSyCLt_erXKZJQf10fJ-YrUy61czJVgHn1oY";
+  static String messagingSenderId = "692680391850";
+  static String projectId = "chathub-1d531";
+  final primaryColor = const Color(0xFFee7b64);
+}
