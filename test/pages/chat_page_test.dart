b31dbd82a0b3d14902dc67e99602dbb5c1188d7f
vassa33 <peggyongoma@gmail.com>
2023-11-27 19:25:39 +0300

    Update dependencies and add chat page test

--- test/pages/chat_page_test.dart
+++ test/pages/chat_page_test.dart
@@ -1,50 +1,60 @@
-import 'package:chathub/widgets/message_tile.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
 import 'package:chathub/pages/chat_page.dart';
-import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:chathub/service/database_service.dart';
+import 'package:firebase_core/firebase_core.dart' as firebase_core;
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
 
-void main() async {
-  // Initialize Firebase outside the test
-  await setupFirebase();
+// Mock DatabaseService class
+class MockDatabaseService extends Mock implements DatabaseService {}
 
-  testWidgets('Chat Page Widgets Test', (tester) async {
+// MockFirebaseApp class
+class MockFirebaseApp extends Mock implements firebase_core.FirebaseApp {
+  MockFirebaseApp() : super();
+}
+
+void main() {
+  late MockDatabaseService mockDatabaseService;
+
+  setUp(() async {
+    await firebase_core.Firebase.initializeApp();
+    mockDatabaseService = MockDatabaseService();
+  });
+
+  testWidgets('chat page test', (tester) async {
+    // Mock Firebase.initializeApp
+    when(firebase_core.Firebase.initializeApp())
+        .thenAnswer((_) async => Future.value(MockFirebaseApp()));
+
     // Build our app and trigger a frame.
     await tester.pumpWidget(
       const MaterialApp(
         home: ChatPage(
-          groupId: 'groupId',
-          groupName: 'groupName',
-          userName: 'userName',
+          groupId: "mockGroupId",
+          groupName: "MockGroup",
+          userName: "MockUser",
         ),
       ),
     );
 
-    // Test for the presence of the app bar title.
-    expect(find.text('groupName'), findsOneWidget);
-
-    // Test for the presence of the input text field.
-    expect(find.byType(TextFormField), findsOneWidget);
-
-    // Test for the presence of the send button.
-    expect(find.byIcon(Icons.send), findsOneWidget);
-
-    // Test for the absence of messages initially.
-    expect(find.byType(MessageTile), findsNothing);
-
-    // Now, let's test some methods.
-
-    // Mock a message and send it.
-    await tester.enterText(find.byType(TextFormField), 'Test message');
+    // Trigger the send message function
     await tester.tap(find.byIcon(Icons.send));
-    await tester.pump();
 
-    // Test if the message is visible after sending.
-    expect(find.text('Test message'), findsOneWidget);
+    // Verify that sendMessage method is called
+    verify(mockDatabaseService
+        .sendMessage("mockGroupId", {'mockKey': 'mockValue'})).called(1);
+
+    // Verify the captured arguments for getChats
+    final capturedArguments = <String>[]; // Declare the list
+    verify(mockDatabaseService.getChats('mockString'))
+        .captured
+        .single
+        .then((arguments) {
+      capturedArguments.addAll(arguments);
+    });
+    verify(mockDatabaseService.getChats('mockString')).called(1);
+
+    // Print the captured arguments for debugging
+    debugPrint('Captured arguments for getChats: ${capturedArguments.first}');
   });
-}
-
-Future<void> setupFirebase() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
 }
